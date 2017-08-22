a57dc8db6a764111da2436c2cb1f9ac92dc8d3a7
TedtheDev <tylerjedmunds@gmail.com>
2017-08-22 15:31:29 -0500

    added more tests to comment_box_test and added logic to comment_box component

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -3,15 +3,14 @@ import App from '../../src/components/app';
 
 // Use describe to group together similar tests
 describe('App', () => {
+  let component;
 
-  // use 'it' to test a single attribute of a target
-  it('shows the correct text', () => {
-    // create an instance of App
-    const component = renderComponent(App);
+  beforeEach(() => {
+    component = renderComponent(App);
+  });
 
-    // use 'expect' to make an 'assertion' about a target
-    expect(component).to.contain('React simple starter');
-
+  it('shows a comment box', () => {
+    expect(component.find('.comment-box')).to.exist;
   });
 
 });
--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -2,12 +2,43 @@ import { renderComponent, expect } from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
+  // scope component variable to be able
+  // to be used in other tests
+  let component;
+
+  beforeEach(() => {
+    // use renderComponent from test_helper to create
+    // an instance of CommentBox
+    component = renderComponent(CommentBox);
+  });
+
+  it('has the correct class', () => {
+    // check component to have css class 'comment-box'
+    expect(component).to.have.class('comment-box');
+  });
+
   it('has a text area', () => {
-    const component = renderComponent(CommentBox);
+    // check if component has textarea
     expect(component.find('textarea')).to.exist;
   });
 
   it('has a button', () => {
-
+    // check if component has a button
+    expect(component.find('button')).to.exist;
   });
+
+  describe('entering some text', () => {
+    beforeEach(() => {
+      component.find('textarea').simulate('change', 'new comment');
+    });
+
+    it('shows that text in the textarea', () => {
+      expect(component.find('textarea')).to.have.value('new comment');
+    });
+
+    it('when submitted, clears the input', () => {
+
+    });
+  });
+
 });
