affe01d0de55376fd68f64ae3c2167f74c7262ce
TedtheDev <tylerjedmunds@gmail.com>
2017-08-22 00:13:29 -0500

    added comment_box components, added comment_box_test boilerplate

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -2,10 +2,16 @@ import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
 // Use describe to group together similar tests
-describe('App')
+describe('App', () => {
 
-// use 'it' to test a single attribute of a target
-it('shows the correct text')
+  // use 'it' to test a single attribute of a target
+  it('shows the correct text', () => {
+    // create an instance of App
+    const component = renderComponent(App);
 
-// use 'expect' to make an 'assertion' about a target
-expect
+    // use 'expect' to make an 'assertion' about a target
+    expect(component).to.contain('React simple starter');
+
+  });
+
+});
