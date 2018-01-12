7df4478bd0a2b4d131b5bbb0d756849ca7a0eea8
TedtheDev <tylerjedmunds@gmail.com>
2018-01-12 15:05:07 -0600

    wired up authenticated creator and reducer to header component

--- HigherOrderComponents/src/components/header.js
+++ HigherOrderComponents/src/components/header.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Header extends Component {
 
     authButton() {
-        return <button>Sign In</button>
+        if(this.props.authenticated) {
+            return <button onClick={() => this.props.authenticate(false)}>Sign out</button>;
+        } else {
+            return <button onClick={() => this.props.authenticate(true)}>Sign In</button>
+        }
     }
 
     render() {
@@ -26,4 +32,10 @@ class Header extends Component {
     }
 };
 
-export default Header;
+function mapStateToProps(state) {
+    return {
+        authenticated: state.authenticated
+    }
+}
+
+export default connect(mapStateToProps, actions)(Header);
