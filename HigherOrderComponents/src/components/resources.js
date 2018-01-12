bc7c7f2cda0578d86379f8d0e0800a948dfb5c6b
TedtheDev <tylerjedmunds@gmail.com>
2018-01-12 00:10:48 -0600

    added Header components with home, resources, signin button; added Resources component for our super special recipe

---
+++ HigherOrderComponents/src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default (props) => {
+    return (
+        <div>
+            Super Special Recipe
+            <ul>
+                <li>1 cup sugar</li>
+                <li>1 cup pepper</li>
+                <li>1 cup salt</li>
+            </ul>
+        </div>
+    )
+};
