fbc6a0e9cb49c7c5dcd4ffec6b61c76cc51579b6
TedtheDev <tylerjedmunds@gmail.com>
2018-01-27 13:52:52 -0600

    added directory Authentication to start new section; added auth directory for server portion of authentication; added index.js with dependencies to create server

---
+++ Authentication/auth/server/index.js
@@ -0,0 +1,23 @@
+// Main starting point of our application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+// App setup
+
+
+// Server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:' + port);
+
+
+// another way to start server
+/*
+app.listen(() => {
+    console.log('Server listening on: ' + port);
+})
+*/
