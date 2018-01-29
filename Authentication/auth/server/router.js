398e72ae243640d8899f66e90d123201c84a67bb
TedtheDev <tylerjedmunds@gmail.com>
2018-01-29 00:46:19 -0600

    added authentication controller with test path

--- Authentication/auth/server/router.js
+++ Authentication/auth/server/router.js
@@ -1,5 +1,5 @@
+const Authentication = require('./controllers/authentication');
+
 module.exports = function(app) {
-    app.get('/', function(req, res) {
-        res.send(['waterbottle', 'phone', 'paper'])
-    });
+    app.post('/signup', Authentication.signup);
 };
