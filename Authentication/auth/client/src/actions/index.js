0a5db5ea9fd0401f7029c52227033cfbcd1bff0b
TedtheDev <tylerjedmunds@gmail.com>
2018-01-31 16:34:01 -0600

    added redux-thunk to to dependencies and added to redux middleware; created template function with dispatch in signinUser action creator

---
+++ Authentication/auth/client/src/actions/index.js
@@ -0,0 +1,17 @@
+
+
+export function signinUser({email, password}) {
+    return function(dispatch) {
+        // Submit email/password to the server
+
+
+        // If request is good...
+        // - Update state to indicate user is authenticated
+        // - Save the JWT Token
+        // - Redirect to the route '/feature'
+
+
+        // If request is bad....
+        // - Show an error to the user
+    }
+};
