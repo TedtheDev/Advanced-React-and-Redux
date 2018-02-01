6884b3e33fa067f569cd00d624ff8c8d838d9693
TedtheDev <tylerjedmunds@gmail.com>
2018-02-01 16:34:59 -0600

    added auth reducer; added action types file; wired up localstorage to save token on success to signin

--- Authentication/auth/client/src/actions/index.js
+++ Authentication/auth/client/src/actions/index.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -6,14 +8,18 @@ export function signinUser({email, password}) {
     return function(dispatch) {
         // Submit email/password to the server
         axios.post(`${ROOT_URL}/signin`, { email, password})
-
-        // If request is good...
-        // - Update state to indicate user is authenticated
-        // - Save the JWT Token
-        // - Redirect to the route '/feature'
-
-
-        // If request is bad....
-        // - Show an error to the user
+            .then(response => {
+                // If request is good...
+                // - Save the JWT Token
+                localStorage.setItem('token', response.data.token);
+                // - Redirect to the route '/feature'
+                browserHistory.push('/feature');
+                // - Update state to indicate user is authenticated
+                dispatch({type: AUTH_USER})
+            })
+            .catch(err => {
+                // If request is bad....
+                // - Show an error to the user
+            });
     }
 };
---
+++ Authentication/auth/client/src/actions/types.js
@@ -0,0 +1,2 @@
+const AUTH_USER = 'auth_user';
+const UNAUTH_USER = 'unauth_user';
