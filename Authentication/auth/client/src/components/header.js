ed10e4c9338794e99b1e7a31a3b86da95e2e9115
TedtheDev <tylerjedmunds@gmail.com>
2018-02-02 16:04:53 -0600

    header mockup

--- Authentication/auth/client/src/components/header.js
+++ Authentication/auth/client/src/components/header.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
-    render() {
+    renderNavBar(action) {
         return (
             <nav className='navbar navbar-light'>
                 <ul className='nav navbar-nav'>
-                    <li className='nav-item'>Sign In</li>
+                    <li className='nav-item'>{action}</li>
                 </ul>
             </nav>
         )
+    };
+    
+    render() {
+        return (
+            (this.props.authenticated) ? this.renderNavBar('Sign Out') : this.renderNavBar('Sign In')
+        )
+        
     }
 }
 
-export default Header;
+function mapStateToProps(state) {
+    return {
+        authenticated: state.auth.authenticated
+    }
+}
+
+export default connect(mapStateToProps)(Header);
