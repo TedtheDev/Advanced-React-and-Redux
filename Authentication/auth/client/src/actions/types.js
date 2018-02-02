aa2061de2c704d2afee9a82950c9529a20004a74
TedtheDev <tylerjedmunds@gmail.com>
2018-02-02 11:17:53 -0600

    added AUTH_ERROR type and case to auth_reducer; wired up error handling message for user on Signin component

--- Authentication/auth/client/src/actions/types.js
+++ Authentication/auth/client/src/actions/types.js
@@ -1,2 +1,3 @@
-const AUTH_USER = 'auth_user';
-const UNAUTH_USER = 'unauth_user';
+export const AUTH_USER = 'auth_user';
+export const UNAUTH_USER = 'unauth_user';
+export const AUTH_ERROR = 'error';
--- Authentication/auth/client/src/components/auth/signin.js
+++ Authentication/auth/client/src/components/auth/signin.js
@@ -10,6 +10,15 @@ class Signin extends Component {
         this.props.signinUser({email, password});
     };
 
+    renderAlert() {
+        if(this.props.errorMessage) {
+            return (
+                <div className='alert alert-danger'>
+                    <strong>Oops!</strong> {this.props.errorMessage}
+                </div>
+            )
+        }
+    }
     render() {
         const { handleSubmit, fields: { email, password }} = this.props;
 
@@ -21,15 +30,22 @@ class Signin extends Component {
                 </fieldset>
                 <fieldset className='form-group'>
                     <label>Password:</label>
-                    <input className='form-control' {...password} />
+                    <input className='form-control' {...password} type='password' />
                 </fieldset>
+                { this.renderAlert()}
                 <button action='submit' className='btn btn-primary'>Sign in</button>
             </form>
         )
     }
 };
 
+function mapStateToProps(state) {
+    return {
+        errorMessage: state.auth.error
+    };
+}
+
 export default reduxForm({
     form: 'signin',
     fields: ['email','password']
-}, null, actions)(Signin);
+}, mapStateToProps, actions)(Signin);
