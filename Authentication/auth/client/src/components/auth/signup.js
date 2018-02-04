6eebd776f150801cc3fef1c1ccaee14be014be02
TedtheDev <tylerjedmunds@gmail.com>
2018-02-03 22:43:08 -0600

    created Signup component; added signup route for signup component

---
+++ Authentication/auth/client/src/components/auth/signup.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import * as actions from '../../actions';
+
+class Signup extends Component {
+    render() {
+        return (
+            <div>Sign up</div>
+        )
+    }
+};
+
+export default Signup;
