873bd0d025814c4d80be64e53fe97c376434c856
TedtheDev <tylerjedmunds@gmail.com>
2018-02-11 00:12:33 -0600

    created Feature componented and created a route to /feature and wired up feature component to route

--- Authentication/auth/client/src/index.js
+++ Authentication/auth/client/src/index.js
@@ -9,6 +9,7 @@ import App from './components/app';
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
 import Signup from './components/auth/signup';
+import Feature from './components/feature';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
@@ -20,6 +21,7 @@ ReactDOM.render(
         <Route path='signin' component={Signin}></Route>
         <Route path='signout' component={Signout}></Route>
         <Route path='signup' component={Signup}></Route>
+        <Route path='feature' component={Feature}></Route>
       </Route>
     </Router>
   </Provider>
