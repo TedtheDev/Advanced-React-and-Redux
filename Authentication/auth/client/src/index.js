96e5062ff06acba35270ffff57cc64e70f58fca3
TedtheDev <tylerjedmunds@gmail.com>
2018-02-11 00:32:33 -0600

    get token when user lands on the page to set authenticated to true if valid token

--- Authentication/auth/client/src/index.js
+++ Authentication/auth/client/src/index.js
@@ -13,11 +13,21 @@ import Feature from './components/feature';
 import RequireAuth from './components/auth/require_auth';
 import Welcome from './components/welcome';
 import reducers from './reducers';
+import { AUTH_USER } from './actions/types';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+// if we have a toekn, consider the user to be signed in
+if(token) {
+  // update application state
+  store.dispatch({ type: AUTH_USER} );
+}
+
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path='/' component={App}>
         <IndexRoute component={Welcome} />
